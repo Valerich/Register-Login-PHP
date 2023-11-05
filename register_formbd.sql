5d70d114f62a508780fa1bb7b4968efb64dc0b07
Bair <valerievich.19@yandex.ru>
2023-11-05 20:01:12 +0700

    update database

--- register_formbd.sql
+++ register_formbd.sql
@@ -3,9 +3,10 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Ноя 05 2023 г., 13:34
+-- Время создания: Ноя 05 2023 г., 15:54
 -- Версия сервера: 8.0.30
 -- Версия PHP: 8.1.9
+
 CREATE DATABASE register_formbd;
 USE register_formbd;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -30,10 +31,10 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `users` (
   `id` int UNSIGNED NOT NULL,
-  `user_name` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
-  `email` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `user_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `tel_number` bigint NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -61,7 +62,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
